838543bbcc498ddd8520feca286a88e274d01830
roshanking04 <107301394+roshanking04@users.noreply.github.com>
2026-03-24 12:10:52 +0530

    Update WebConfig.java

--- src/main/java/com/product/config/WebConfig.java
+++ src/main/java/com/product/config/WebConfig.java
@@ -7,12 +7,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Override
+  /* @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // Serve uploaded images — works on both local and Railway
         String uploadPath = System.getProperty("user.dir") + "/uploads/";
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:" + uploadPath)
                 .addResourceLocations("file:uploads/");
+    }*/
+     @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        String uploadPath = Paths.get("uploads").toAbsolutePath().toUri().toString();
+
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations(uploadPath);
     }
 }
