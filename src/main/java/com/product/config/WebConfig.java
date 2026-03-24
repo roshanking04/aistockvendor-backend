7c79c0c94d3ed237308a342bf0790df9a76dd980
roshanking04 <107301394+roshanking04@users.noreply.github.com>
2026-03-24 12:31:36 +0530

    Update WebConfig.java

--- src/main/java/com/product/config/WebConfig.java
+++ src/main/java/com/product/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.product.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import java.nio.file.Paths;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
